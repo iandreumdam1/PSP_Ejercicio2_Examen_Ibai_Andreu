7a2341556fd0e502e36ad327af8a80ec0b6b1f7b
iandreumdam1 <iandreumdam1@gmail.com>
2021-11-16 11:12:24 +0100

    Agregar archivos de proyecto.

---
+++ PSP_Ejercicio2_Examen_Ibai_Andreu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PSP_Ejercicio2_Examen_Ibai_Andreu
+{
+    class Program
+    {
+        public static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var th = new Thread(hilosDeCoser);
+                th.Start();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void hilosDeCoser()
+        { 
+            Console.WriteLine("Ahora saco un hilo de color (No se soy daltonico)");
+        }
+       
+    }
+}
